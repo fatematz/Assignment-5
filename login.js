8ebf25b9764c5bca102e79c712572b4592f5dd53
Fatema <fatematz486@gmail.com>
2026-03-06 22:20:47 +0600

    Initial commit

---
+++ login.js
@@ -0,0 +1,24 @@
+document.getElementById('login-btn').addEventListener("click", function() {
+    let numberInput = document.getElementById('mobile')
+    let contactNumber = numberInput.value
+     
+    let inputDigit=document.getElementById("digit");
+    let digitNumber=inputDigit.value;
+
+    
+    if(contactNumber=="admin"&&digitNumber=="admin123") {
+        alert("Login successfully")
+        window.location.assign('issue-Tracker.html')
+        
+        // if(window.location.protocol ='http') {
+        //     window.location.assign('issue-Tracker.html')
+        // }
+        //  else {
+        //     window.location.assign('issue-Tracker.html')
+        // }
+    }
+    else {
+        alert("Your Information Is Incorrect")
+        return;
+    }
+})
