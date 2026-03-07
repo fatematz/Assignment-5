55c583d9a26c5f343ea533c57093f46c5bd10b74
Fatema <fatematz486@gmail.com>
2026-03-07 17:22:40 +0600

    design dynamic cards

--- script.js
+++ script.js
@@ -17,10 +17,15 @@ function toggleStyle(id) {
     selected.classList.add('bg-blue-700', 'text-white')
 }
 
+let allIssues=[];
+
 const LoadIssues=() => {
     fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
         .then(Response => Response.json())
-    .then(json => displayIssue( json.data ))
+        .then(json => {
+            allIssues=json.data;
+        displayIssue( json.data )
+    })
 }
 
 const displayIssue=(issues) => {
@@ -32,18 +37,26 @@ const displayIssue=(issues) => {
        
         const addElement=document.createElement("div")
         addElement.innerHTML=`
+             <div class=" shadow-[0px_0px_15px_rgba(0,0,0,0.5)] py-[20px] px-[10px] space-y-3 h-[417px] rounded-xl">
              <div class="">
-           <h2>${issue.title}</h2>
-        <h3>${issue.description}</h3>
+             <button class="w-[70px] h-[30px] rounded-full bg-red-200 text-red-700   ml-[190px] hover:bg-gray-500 hover:text-white hover:border-none">${issue.priority}</button>
+             </div>
+             <div class="">
+           <h2 class="text-[20px] font-bold">${issue.title}</h2>
+           </div>
+           <div class="">
+        <h3 class="text-[18px] text-[##64748B]" >${issue.description}</h3>
+        </div>
+        <div class="border-b-1 flex gap-[20px] py-[20px]">
+            <button class="w-[120px] h-[30px] rounded-full bg-red-200 text-red-700 border border-red-500 hover:bg-blue-500 hover:text-white hover:border-none">${issue.labels[0]}</button>
+            <button class="w-[120px] h-[30px] rounded-full bg-[#eed77b] text-red-700 border border-[#cfa90e] hover:bg-blue-500 hover:text-white hover:border-none">${issue.labels[1]}</button>
+        </div>
+       
         <div class="">
-            <span>${issue.labels[0]}</span>
-            <span>${issue.labels[1]}</span>
+            <p class="text-[20px]">${issue.author}</p>
+            <p class="text-[20px]">${issue.createdAt}</p>
         </div>
-        <div class="">
-            <p>${issue.author}</p>
-            <p>${issue.createdAt}</p>
-        </div>
-        </div>
+         </div>
         
         `
 
