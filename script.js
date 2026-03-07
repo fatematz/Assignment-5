0433714aa19257000fc81e520a03f8929596120d
Fatema <fatematz486@gmail.com>
2026-03-07 11:50:29 +0600

    Fetch issues from API and display them in the UI

--- script.js
+++ script.js
@@ -2,39 +2,57 @@ const allBtn = document.getElementById('AllBtn')
 const openBtn = document.getElementById('openBtn')
 const closeBtn=document.getElementById('closeBtn')
 
-// const buttons = [allBtn, openBtn, closeBtn];
-
-// function toggleStyle (id) {
-
-//         buttons.forEach(btn => {
-//         btn.classList.remove('bg-blue-600', 'text-white');
-//         btn.classList.add('bg-gray-200', 'text-black');
-//     });
-
-//     let selected=document.getElementById(id)
-//     selected.classList.remove('bg-gray-200', 'text-black');
-//     selected.classList.add('bg-blue-600', 'text-white');
-// }
-
 function toggleStyle(id) {
-    // adding bg day for all
+   
     allBtn.classList.add('bg-base-200', 'text-black')
     openBtn.classList.add('bg-base-200', 'text-black')
     closeBtn.classList.add('bg-base-200', 'text-black')
 
-    // if any button has black then remove
     allBtn.classList.remove('bg-blue-700', 'text-white')
     openBtn.classList.remove('bg-blue-700', 'text-white')
     closeBtn.classList.remove('bg-blue-700', 'text-white')
 
-    // console.log( id )
-
     let selected = document.getElementById(id)
-    // console.log(selected, "id select")
-
-    //  previous style remove
     selected.classList.remove('bg-base-200', 'text-black')
-
-    // adding black for the current button / adding new style
     selected.classList.add('bg-blue-700', 'text-white')
 }
+
+const LoadIssues=() => {
+    fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
+        .then(Response => Response.json())
+    .then(json => displayIssue( json.data ))
+}
+
+const displayIssue=(issues) => {
+    console.log( issues )
+    const getId=document.getElementById("issue-container")
+    getId.innerHTML="";
+
+    for(let issue of issues) {
+       
+        const addElement=document.createElement("div")
+        addElement.innerHTML=`
+             <div class="">
+           <h2>${issue.title}</h2>
+        <h3>${issue.description}</h3>
+        <div class="">
+            <span>${issue.labels[0]}</span>
+            <span>${issue.labels[1]}</span>
+        </div>
+        <div class="">
+            <p>${issue.author}</p>
+            <p>${issue.createdAt}</p>
+        </div>
+        </div>
+        
+        `
+
+         getId.append(addElement)
+    }
+
+   
+    
+}
+
+LoadIssues()
+
